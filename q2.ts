c15e12edc3ecc1de529b04a1770e8d222f6cfdec
xtxme <TT.icy013@gmail.com>
2024-07-10 17:40:55 +0700

    Q2 Done

--- q2.ts
+++ q2.ts
@@ -4,6 +4,19 @@ import axios from "axios";
 /* assign interface/type to the function definition properly */
 const getTodo = async (todoId: number) => {
   /* Your code here */
+  try {
+    const res1 = await axios.get(`https://jsonplaceholder.typicode.com/todos/${todoId}`);
+    const res2 = await axios.get(`https://jsonplaceholder.typicode.com/users/${res1.data.userId}`);
+    return {
+      owner : res2.data.name,
+      title : res1.data.title,
+      completed : res1.data.completed
+    }
+  }
+
+  catch (error) {
+    return `INVALID TODO ID`;
+  }
 };
 
 //test case
@@ -17,3 +30,5 @@ getTodo(input2).then((result) => console.log(result));
 getTodo(input3).then((result) => console.log(result));
 
 export default getTodo;
+
+//นางสาวธนพร ตั้งผดุงสุข 660610762
